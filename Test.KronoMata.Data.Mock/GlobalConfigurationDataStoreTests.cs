7881b3813c337118436ad5fa676f868935afa55d
Bill Nickel <bill.nickel@gmail.com>
2023-07-25 11:44:21 -0700

    Finished test coverage of MockGlobalConfigurationDataStore

--- Test.KronoMata.Data.Mock/GlobalConfigurationDataStoreTests.cs
+++ Test.KronoMata.Data.Mock/GlobalConfigurationDataStoreTests.cs
@@ -94,20 +94,93 @@ namespace Test.KronoMata.Data.Mock
         [Test()]
         public void Can_GetAll()
         {
-            Assert.Fail();
+            var now = DateTime.Now;
+            const int count = 10;
+
+            for (int x = 0; x < count; x++)
+            {
+                var globalConfiguration = new GlobalConfiguration()
+                {
+                    Category = "TestCategory",
+                    Name = "TestName",
+                    Value = "TestValue",
+                    IsAccessibleToPlugins = true,
+                    InsertDate = now,
+                    UpdateDate = now
+                };
+
+                _provider.GlobalConfigurationDataStore.Create(globalConfiguration);
+            }
+
+            var all = _provider.GlobalConfigurationDataStore.GetAll();
+            Assert.That(all.Count, Is.EqualTo(count));
         }
 
         [Test()]
         public void Can_GetByCategory()
         {
-            Assert.Fail();
+            var now = DateTime.Now;
+            const int count = 10;
+
+            for (int x = 1; x <= count; x++)
+            {
+
+                var globalConfiguration1 = new GlobalConfiguration()
+                {
+                    Category = "TestCategory1",
+                    Name = $"TestName{x}",
+                    Value = $"TestValue{x}",
+                    IsAccessibleToPlugins = true,
+                    InsertDate = now,
+                    UpdateDate = now
+                };
+
+                _provider.GlobalConfigurationDataStore.Create(globalConfiguration1);
+
+                var globalConfiguration2 = new GlobalConfiguration()
+                {
+                    Category = "TestCategory2",
+                    Name = $"TestName{x}",
+                    Value = $"TestValue{x}",
+                    IsAccessibleToPlugins = true,
+                    InsertDate = now,
+                    UpdateDate = now
+                };
+                
+                _provider.GlobalConfigurationDataStore.Create(globalConfiguration2);
+            }
+
+            var category1 = _provider.GlobalConfigurationDataStore.GetByCategory("TestCategory1");
+
+            Assert.That(category1.Count, Is.EqualTo(count));
         }
 
         [Test()]
         public void Can_GetByCategoryAndName()
         {
-            Assert.Fail();
-        }
+            var now = DateTime.Now;
+            const int count = 10;
 
+            for (int x = 0; x < count; x++)
+            {
+                var globalConfiguration = new GlobalConfiguration()
+                {
+                    Category = "TestCategory",
+                    Name = $"TestName{x + 1}",
+                    Value = $"TestValue{x + 1}",
+                    IsAccessibleToPlugins = true,
+                    InsertDate = now,
+                    UpdateDate = now
+                };
+
+                _provider.GlobalConfigurationDataStore.Create(globalConfiguration);
+            }
+
+            var one = _provider.GlobalConfigurationDataStore.GetByCategoryAndName("TestCategory", "TestName3");
+
+            Assert.IsNotNull(one);
+            Assert.That(one.Name, Is.EqualTo("TestName3"));
+            Assert.That(one.Value, Is.EqualTo("TestValue3"));
+        }
     }
 }
