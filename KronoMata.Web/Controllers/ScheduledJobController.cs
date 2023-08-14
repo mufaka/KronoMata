5425c5d346f4404a0dceacdfecf740c08279de24
Bill Nickel <bill.nickel@gmail.com>
2023-08-13 21:14:52 -0700

    Created reusable view and model for ScheduledJob creation and updating.

--- KronoMata.Web/Controllers/ScheduledJobController.cs
+++ KronoMata.Web/Controllers/ScheduledJobController.cs
@@ -56,5 +56,117 @@ namespace KronoMata.Web.Controllers
                 return new ObjectResult(ex.Message) { StatusCode = 500 };
             }
         }
+
+        private void PopulateCommonProperties(ScheduledJobSaveViewModel model)
+        {
+            var hosts = DataStoreProvider.HostDataStore.GetAll()
+                .Where(h => h.IsEnabled)
+                .OrderBy(h => h.MachineName)
+                .ToList();
+
+            var allHost = new Model.Host()
+            {
+                Id = 0,
+                MachineName = "<All>"
+            };
+
+            model.Plugins = DataStoreProvider.PluginMetaDataDataStore.GetAll().OrderBy(p => p.Name).ToList();
+
+            hosts.Insert(0, allHost);
+        }
+
+        public ActionResult Create()
+        {
+            var model = new ScheduledJobSaveViewModel()
+            {
+                ViewName = "Scheduled Job Create",
+#pragma warning disable CS8601 // Possible null reference assignment.
+                ActionUrl = Url.Action("Create", "ScheduledJob")
+#pragma warning restore CS8601 // Possible null reference assignment.
+            };
+
+            try
+            {
+                PopulateCommonProperties(model);
+            }
+            catch (Exception ex)
+            {
+                LogException(model, ex);
+                _logger.LogError(ex, "Error loading data for View {viewname}", model.ViewName);
+            }
+
+            return View("Save", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ScheduledJobSaveViewModel model)
+        {
+            model.ViewName = "Scheduled Job Create";
+#pragma warning disable CS8601 // Possible null reference assignment.
+            model.ActionUrl = Url.Action("Create", "ScheduledJob");
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            try
+            {
+                PopulateCommonProperties(model);
+            }
+            catch (Exception ex)
+            {
+                LogException(model, ex);
+                _logger.LogError(ex, "Error loading data for View {viewname}", model.ViewName);
+                return View("Save", model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var model = new ScheduledJobSaveViewModel() {
+                ViewName = "Scheduled Job Update",
+#pragma warning disable CS8601 // Possible null reference assignment.
+                ActionUrl = Url.Action("Update", "ScheduledJob")
+#pragma warning restore CS8601 // Possible null reference assignment.
+            };
+
+            try
+            {
+                PopulateCommonProperties(model);
+                model.ScheduledJob = DataStoreProvider.ScheduledJobDataStore.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                LogException(model, ex);
+                _logger.LogError(ex, "Error loading data for View {viewname}", model.ViewName);
+            }
+
+            return View("Save", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(ScheduledJobSaveViewModel model)
+        {
+            model.ViewName = "Scheduled Job Update";
+#pragma warning disable CS8601 // Possible null reference assignment.
+            model.ActionUrl = Url.Action("Update", "ScheduledJob");
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            try
+            {
+                PopulateCommonProperties(model);
+                model.ScheduledJob = DataStoreProvider.ScheduledJobDataStore.GetById(model.ScheduledJob.Id);
+            }
+            catch (Exception ex)
+            {
+                LogException(model, ex);
+                _logger.LogError(ex, "Error loading data for View {viewname}", model.ViewName);
+                return View("Save", model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ KronoMata.Web/Models/ScheduledJobSaveViewModel.cs
@@ -0,0 +1,12 @@
+﻿using KronoMata.Model;
+
+namespace KronoMata.Web.Models
+{
+    public class ScheduledJobSaveViewModel : BaseViewModel
+    {
+        public string ActionUrl { get; set; } = String.Empty;
+        public List<PluginMetaData> Plugins { get; set; } = new List<PluginMetaData>();
+        public List<Model.Host> Hosts { get; set; } = new List<Model.Host>();
+        public ScheduledJob ScheduledJob { get; set; } = new ScheduledJob();
+    }
+}
