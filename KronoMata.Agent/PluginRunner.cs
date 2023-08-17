8fa9f9d029201d32a36fbf50a540e8d0fe81e835
Bill Nickel <bill.nickel@gmail.com>
2023-08-16 19:46:36 -0700

    Begin work on the Agent.

---
+++ KronoMata.Agent/PluginRunner.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KronoMata.Agent
+{
+    internal class PluginRunner : IHostedService
+    {
+        private readonly ILogger<PluginRunner> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly PeriodicTimer _periodicTimer = new(TimeSpan.FromMinutes(1));
+
+        public PluginRunner(ILogger<PluginRunner> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("KronoMata Agent started.");
+
+            await ExecuteAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("KronoMata Agent stopped.");
+            _periodicTimer.Dispose();
+            return Task.CompletedTask;
+        }
+
+        private async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (await _periodicTimer.WaitForNextTickAsync(cancellationToken)
+                && !cancellationToken.IsCancellationRequested)
+                {
+                    //CheckForJobs();
+                    _logger.LogInformation("{time} Agent checking for jobs.", DateTime.Now.ToString("O"));
+                }
+            }
+            catch (OperationCanceledException) { }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected errror executing agent. {ex.Message", ex.Message);
+            }
+        }
+    }
+}
---
+++ KronoMata.Agent/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KronoMata.Agent
+{
+    internal class Program
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        static async Task Main(string[] args)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            try
+            {
+                IConfiguration configuration = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetCurrentDirectory())
+                      .AddJsonFile("appsettings.json", false)
+                      .Build();
+
+                var agentHost = new HostBuilder()
+                    .ConfigureHostConfiguration(host => { })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddLogging(builder =>
+                        {
+                            builder.ClearProviders();
+                            builder.AddConfiguration(configuration.GetSection("Logging"));
+                            builder.AddConsole();
+                            builder.AddDebug();
+                        });
+                        services.AddSingleton(configuration);
+                        services.AddSingleton<IHostedService, PluginRunner>();
+                    })
+                    .UseConsoleLifetime()
+                    .Build();
+
+                // suppressing this warning because we wan't the call to be async
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                agentHost.RunAsync();
+
+                Console.ReadLine();
+
+                agentHost.StopAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
