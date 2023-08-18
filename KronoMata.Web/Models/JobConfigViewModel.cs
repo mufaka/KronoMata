fe28731dd8a33f1400dc195be92967d4728e5d4e
Bill Nickel <bill.nickel@gmail.com>
2023-08-17 20:39:53 -0700

    Begin work on ViewComponent for editing Scheduled Job configuration based on the PluginConfiguration DataType.

---
+++ KronoMata.Web/Models/JobConfigViewModel.cs
@@ -0,0 +1,16 @@
+﻿using KronoMata.Model;
+
+namespace KronoMata.Web.Models
+{
+    public class JobConfigViewModel
+    {
+        public JobConfigViewModel(PluginConfiguration pluginConfiguration, ConfigurationValue configurationValue)
+        {
+            PluginConfiguration = pluginConfiguration;
+            ConfigurationValue = configurationValue;
+        }
+
+        public PluginConfiguration PluginConfiguration { get; set; }
+        public ConfigurationValue ConfigurationValue { get; set; }
+    }
+}
---
+++ KronoMata.Web/ViewComponents/JobConfigViewComponent.cs
@@ -0,0 +1,59 @@
+﻿using KronoMata.Data;
+using KronoMata.Model;
+using KronoMata.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KronoMata.Web.ViewComponents
+{
+    /// <summary>
+    /// Renders form controls based on the PluginConfiguration
+    /// DataType.
+    /// </summary>
+    public class JobConfigViewComponent : ViewComponent
+    {
+        private readonly IDataStoreProvider _dataStoreProvider;
+
+        public JobConfigViewComponent(IDataStoreProvider dataStoreProvider)
+        {
+            _dataStoreProvider = dataStoreProvider;
+        }
+
+        public IViewComponentResult Invoke(ConfigurationValue configurationValue)
+        {
+            var pluginConfiguration = _dataStoreProvider.PluginConfigurationDataStore.GetById(configurationValue.PluginConfigurationId);
+            var model = new JobConfigViewModel(pluginConfiguration, configurationValue);
+
+
+            // switch the 'View' based on the PluginConfiguration DataType.
+            var view = "ConfigTextBox";
+
+            switch (pluginConfiguration.DataType)
+            {
+                case Public.ConfigurationDataType.Boolean:
+                    view = "ConfigCheckBox";
+                    break;
+                case Public.ConfigurationDataType.DateTime:
+                    view = "ConfigDateTime";
+                    break;
+                case Public.ConfigurationDataType.Decimal:
+                case Public.ConfigurationDataType.Integer:
+                    view = "ConfigNumeric";
+                    break;
+                case Public.ConfigurationDataType.Password:
+                    view = "ConfigPassword";
+                    break;
+                case Public.ConfigurationDataType.String:
+                    view = "ConfigTextBox";
+                    break;
+                case Public.ConfigurationDataType.Text:
+                    view = "ConfigTextArea";
+                    break;
+            }
+
+            // TODO: Remove this, just testing.
+            view = "ConfigCheckBox";
+
+            return View(view, model);
+        }
+    }
+}
