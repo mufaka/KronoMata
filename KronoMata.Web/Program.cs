d379925372e463b97bb72ccc8ae0931275ff626a
Bill Nickel <bill.nickel@gmail.com>
2023-08-23 09:00:12 -0700

    Use configuration for SQLite connection string.

--- KronoMata.Web/Program.cs
+++ KronoMata.Web/Program.cs
@@ -3,6 +3,7 @@ using KronoMata.Data;
 using KronoMata.Data.Mock;
 using KronoMata.Data.SQLite;
 using KronoMata.Model.Validation;
+using System.Reflection;
 
 namespace KronoMata.Web
 {
@@ -14,12 +15,29 @@ namespace KronoMata.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            
             //builder.Services.AddSingleton(MockDatabase.Instance.DataStoreProvider);
             builder.Services.AddSingleton<IDataStoreProvider>(new SQLiteDataStoreProvider());
             builder.Services.AddValidatorsFromAssemblyContaining<ScheduledJobValidator>();
 
-            var databasePath = Path.Combine("Database", "KronoMata.db");
-            SQLiteDataStoreBase.ConnectionString = $"Data Source={databasePath};Pooling=True;Cache Size=4000;Page Size=1024;FailIfMissing=True;Journal Mode=Off;";
+
+            var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .Build();
+
+            var workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var databaseRelativePath = Path.Combine(config["KronoMata:SQLite:DatabaseRootPath"], config["KronoMata:SQLite:DatabaseFileName"]);
+#pragma warning disable CS8604 // Possible null reference argument.
+            var databasePath = Path.Combine(workingDirectory, databaseRelativePath);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            if (!File.Exists(databasePath))
+            {
+                throw new ArgumentException($"Unable to find database at {databasePath}");
+            }
+
+            var connectionString = $"Data Source={databasePath};{config["KronoMata:SQLite:DatabaseOptions"]}";
+            SQLiteDataStoreBase.ConnectionString = connectionString;
 
             var app = builder.Build();
 
